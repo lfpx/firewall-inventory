476d6fd10fbf137f3faa5624fc66112692ba0e52
Luiz Felipe Peixoto <88688711+lfpx@users.noreply.github.com>
2021-12-21 16:34:50 +0000

    Update host code (#4)

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -17,7 +17,8 @@ class TestBase(TestCase):
     def setUp(self):
         # Will be called before every test
         db.create_all()
-        host = Hosts(host_name="Test Host 1", host_ip="1.1.1.1")
+        host = Hosts(host_name="Test_Host_1", host_ip="1.1.1.1")
+        rule = Rules(port=80, allow=True, host=host)
         db.session.add(host)
         db.session.commit()
 
@@ -34,6 +35,10 @@ class TestViews(TestBase):
 
     def test_create_host_get(self):
         response = self.client.get(url_for('create_host'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_host_get(self):
+        response = self.client.get(url_for('update_host', id=1))
         self.assertEqual(response.status_code, 200)
 
 class TestRead(TestBase):
@@ -57,6 +62,8 @@ class TestCreate(TestBase):
     # This test is creating a dependency between the assertion
     # and the layout of the Home page, if the layout changes and it doesn't
     # show the results anymore this test will fail
+    # The previous test doesn't present this problem because it's reading
+    # its output directly from the db instead of the server response
     def test_create_host_redirect(self):
         response = self.client.post(
             url_for('create_host'), 
@@ -65,3 +72,15 @@ class TestCreate(TestBase):
         )
         self.assertIn('Test_Host_3', str(response.data))
         self.assertIn('3.3.3.3', str(response.data))
+
+class TestUpdate(TestBase):
+
+    def test_update_host(self):
+        response = self.client.post(
+            url_for('update_host', id=1), 
+            json={'host_name':'Test_Host_4','host_ip':'1.1.1.4'},
+            follow_redirects=True
+        )
+        new_host = Hosts.query.get(1)
+        self.assertEqual('Test_Host_4', new_host.host_name)
+        self.assertEqual('1.1.1.4', new_host.host_ip)
