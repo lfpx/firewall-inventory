4e1c98501d4643f583877ed49427e2fcb51f27af
Luiz Felipe Peixoto <88688711+lfpx@users.noreply.github.com>
2021-12-22 09:15:43 +0000

    Feature: Create rule (#7)

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,6 @@
 from application import app, db
 from application.models import Hosts, Rules
-from application.forms import HostForm
+from application.forms import HostForm, RuleForm
 from flask import render_template, request, redirect, url_for
 
 @app.route('/')
@@ -19,7 +19,7 @@ def create_host():
         db.session.commit()
         return redirect(url_for("home"))
 
-    return render_template("create_host.html", title="Add a Host", form=form)
+    return render_template("host_form.html", title="Add a Host", form=form)
 
 @app.route('/update-host/<int:id>', methods=['GET','POST'])
 def update_host(id):
@@ -33,7 +33,7 @@ def update_host(id):
         db.session.commit()
         return redirect(url_for("home"))
 
-    return render_template("create_host.html", title="Update a Host", form=form)
+    return render_template("host_form.html", title="Update a Host", form=form)
 
 @app.route('/delete-host/<int:id>')
 def delete_host(id):
@@ -41,3 +41,17 @@ def delete_host(id):
     db.session.delete(host)
     db.session.commit()
     return redirect(url_for("home"))
+
+@app.route('/create-rule/<int:id>', methods=['GET','POST'])
+def create_rule(id):
+    # host = Hosts.query.get(id)
+    form = RuleForm()
+    form.submit.label.text = "Add Rule"
+
+    if request.method=='POST':
+        new_rule = Rules(port=form.port.data, allow=form.allow.data, host_id=id)
+        db.session.add(new_rule)
+        db.session.commit()
+        return redirect(url_for("home"))
+
+    return render_template("rule_form.html", title="Add a Rule", form=form)
