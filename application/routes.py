feb1bd0e9915da4dd068bdb9079047fe36b544b1
lfpx <luizfpx@gmail.com>
2021-12-22 10:57:43 +0000

    Update rule feature

--- application/routes.py
+++ application/routes.py
@@ -21,9 +21,9 @@ def create_host():
 
     return render_template("host_form.html", title="Add a Host", form=form)
 
-@app.route('/update-host/<int:id>', methods=['GET','POST'])
-def update_host(id):
-    host = Hosts.query.get(id)
+@app.route('/update-host/<int:host_id>', methods=['GET','POST'])
+def update_host(host_id):
+    host = Hosts.query.get(host_id)
     form = HostForm(host_name=host.host_name, host_ip=host.host_ip)
     form.submit.label.text="Update Host"
 
@@ -35,23 +35,37 @@ def update_host(id):
 
     return render_template("host_form.html", title="Update a Host", form=form)
 
-@app.route('/delete-host/<int:id>')
-def delete_host(id):
-    host = Hosts.query.get(id)
+@app.route('/delete-host/<int:host_id>')
+def delete_host(host_id):
+    host = Hosts.query.get(host_id)
     db.session.delete(host)
     db.session.commit()
     return redirect(url_for("home"))
 
-@app.route('/create-rule/<int:id>', methods=['GET','POST'])
-def create_rule(id):
+@app.route('/create-rule/<int:host_id>', methods=['GET','POST'])
+def create_rule(host_id):
     # host = Hosts.query.get(id)
     form = RuleForm()
     form.submit.label.text = "Add Rule"
 
     if request.method=='POST':
-        new_rule = Rules(port=form.port.data, allow=form.allow.data, host_id=id)
+        new_rule = Rules(port=form.port.data, allow=form.allow.data, host_id=host_id)
         db.session.add(new_rule)
         db.session.commit()
         return redirect(url_for("home"))
 
     return render_template("rule_form.html", title="Add a Rule", form=form)
+
+@app.route('/update-rule/<int:rule_id>', methods=['GET','POST'])
+def update_rule(rule_id):
+    rule = Rules.query.get(rule_id)
+    form = RuleForm(port=rule.port, allow=rule.allow)
+    form.submit.label.text="Update Rule"
+
+    if request.method=='POST':
+        rule.port = form.port.data
+        rule.allow = form.allow.data
+        db.session.commit()
+        return redirect(url_for("home"))
+
+    return render_template("rule_form.html", title="Update a Rule", form=form)
