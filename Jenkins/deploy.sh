a02201c652adc0fc5405bd7139d8dca3c9d2f08e
lfpx <luizfpx@gmail.com>
2021-12-27 14:41:47 +0000

    Create Jenkins development pipeline #21

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo '# Copying docker_compose.yaml to deployment server'
-scp ./run_app.sh ../Docker/docker-compose.yaml ${DEPLOYMENT_SERVER}:${DEPLOYMENT_PATH}
+echo "# Copying docker_compose.yaml to deployment server ${DEPLOYMENT_SERVER}"
+scp Jenkins/run_app.sh Docker/docker-compose.yaml ${DEPLOYMENT_SERVER}:${DEPLOYMENT_PATH}
 
-echo '# Starting app on remote server'
+echo "# Starting app on remote server ${DEPLOYMENT_SERVER}"
 ssh ${DEPLOYMENT_SERVER} source ${DEPLOYMENT_PATH}/run_app.sh firewall-inventory
